c6939f0893355836025994d91113a92b82b8205f
xindizhiyin <Jack@jackdembp.lan>
2014-08-15 14:04:20 +0800

    添加label

--- ZpzchinaMobile/NewProjectView/ClearFireCell.m
+++ ZpzchinaMobile/NewProjectView/ClearFireCell.m
@@ -26,30 +26,33 @@
         [arrowImage2 setImage:[UIImage imageNamed:@"新建项目5_09.png"]];
         [self addSubview:arrowImage2];
         
-        NSArray *titleArray = @[@"消防",@"景观绿化"];
+
+        
+        UIButton *tempBtn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+        tempBtn1.frame = CGRectMake(0,10, 80, 30);
+        tempBtn1.titleLabel.textAlignment = NSTextAlignmentLeft;
+        [tempBtn1 setTitle:@"消防" forState:UIControlStateNormal];
+        [tempBtn1 setTitleColor:BlueColor forState:UIControlStateNormal];
+        [tempBtn1 addTarget:self action:@selector(tempBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:tempBtn1];
+        UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(130,10, 120, 30)];
+        [self addSubview:label1];
+        
+        UIButton *tempBtn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+        tempBtn2.frame = CGRectMake(15,10+50, 80, 30);
+        tempBtn2.titleLabel.textAlignment = NSTextAlignmentLeft;
+        [tempBtn2 setTitle:@"景观绿化" forState:UIControlStateNormal];
+        [tempBtn2 setTitleColor:BlueColor forState:UIControlStateNormal];
+        [tempBtn2 addTarget:self action:@selector(tempBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:tempBtn2];
+        UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(130,10+50, 120, 30)];
+        [self addSubview:label2];
+        
         for (int i =0; i<2; i++) {
-            
-            
             
             UIImageView *lingImage = [[UIImageView alloc] initWithFrame:CGRectMake(20, 50*(i+1), 280, 1)];
             [lingImage setImage:[UIImage imageNamed:@"新建项目5_27.png"]];
             [self addSubview:lingImage];
-            
-            UIButton *tempBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-            if (i==0) {
-                tempBtn.frame = CGRectMake(0,10+50*i, 80, 30);
-            }else{
-                tempBtn.frame = CGRectMake(15,10+50*i, 80, 30);
-
-            }
-
-
-            tempBtn.titleLabel.textAlignment = NSTextAlignmentLeft;
-            [tempBtn setTitle:[titleArray objectAtIndex:i] forState:UIControlStateNormal];
-            [tempBtn setTitleColor:BlueColor forState:UIControlStateNormal];
-            [tempBtn addTarget:self action:@selector(tempBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
-            [self addSubview:tempBtn];
-            
             
         }
         
--- ZpzchinaMobile/NewProjectView/WeakElectricityCell.m
+++ ZpzchinaMobile/NewProjectView/WeakElectricityCell.m
@@ -15,7 +15,6 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
-            NSArray *titleArray = @[@"弱电安装",@"装修情况",@"装修进度"];
             for (int i =0; i<3; i++) {
                 
                 UIImageView *arrowImage = [[UIImageView alloc] initWithFrame:CGRectMake(100,50*i+16, 10, 18)];
@@ -26,15 +25,40 @@
                 [lingImage setImage:[UIImage imageNamed:@"新建项目5_27.png"]];
                 [self addSubview:lingImage];
                 
-                UIButton *tempBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-                tempBtn.frame = CGRectMake(15,10+50*i, 80, 30);
-                tempBtn.tag = i;
                 
-                [tempBtn setTitle:[titleArray objectAtIndex:i] forState:UIControlStateNormal];
-                [tempBtn setTitleColor:BlueColor forState:UIControlStateNormal];
-                [tempBtn addTarget:self action:@selector(tempBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
-                [self.contentView addSubview:tempBtn];
         }
+        
+        UIButton *tempBtn1 = [UIButton buttonWithType:UIButtonTypeCustom];
+        tempBtn1.frame = CGRectMake(15,10, 80, 30);
+        [tempBtn1 setTitle:@"弱电安装" forState:UIControlStateNormal];
+        [tempBtn1 setTitleColor:BlueColor forState:UIControlStateNormal];
+        [tempBtn1 addTarget:self action:@selector(tempBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+        [self.contentView addSubview:tempBtn1];
+        
+        UILabel *label1 = [[UILabel alloc] initWithFrame:CGRectMake(130,10, 120, 30)];
+        [self addSubview:label1];
+        
+        UIButton *tempBtn2 = [UIButton buttonWithType:UIButtonTypeCustom];
+        tempBtn2.frame = CGRectMake(15,10+50, 80, 30);
+        [tempBtn2 setTitle:@"装修情况" forState:UIControlStateNormal];
+        [tempBtn2 setTitleColor:BlueColor forState:UIControlStateNormal];
+        [tempBtn2 addTarget:self action:@selector(tempBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+        [self.contentView addSubview:tempBtn2];
+        
+        UILabel *label2 = [[UILabel alloc] initWithFrame:CGRectMake(130,10+50, 120, 30)];
+        [self addSubview:label2];
+
+        
+        UIButton *tempBtn3 = [UIButton buttonWithType:UIButtonTypeCustom];
+        tempBtn3.frame = CGRectMake(15,10+100, 80, 30);
+        [tempBtn3 setTitle:@"装修进度" forState:UIControlStateNormal];
+        [tempBtn3 setTitleColor:BlueColor forState:UIControlStateNormal];
+        [tempBtn3 addTarget:self action:@selector(tempBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+        [self.contentView addSubview:tempBtn3];
+        
+        UILabel *label3 = [[UILabel alloc] initWithFrame:CGRectMake(130,10+100, 120, 30)];
+        [self addSubview:label3];
+
         
         
         
